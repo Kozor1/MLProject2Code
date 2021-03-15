8424d61c334b3bcca1b302b0e3d0dfa7e8a15d7d
Kozor1 <r.m.lavery@hotmail.co.uk>
2021-03-15 01:07:34 +0000

    Update KNComparison.py

--- KNComparison.py
+++ KNComparison.py
@@ -5,12 +5,12 @@ import mlrose_hiive as mlr
 import time
 random_seed = 17
 
-size = 201
+size = 101
 
 x_ax = []
 rhc_times, rhc_fitness, sa_times, sa_fitness, ga_times, ga_fitness, mi_times, mi_fitness = [], [], [], [], [], [], [], []
 
-for n in range(20, size, 20):
+for n in range(10, size, 10):
 
     x_ax.append(n)
 
